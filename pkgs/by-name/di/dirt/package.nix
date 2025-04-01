f86c9298ff6df907770108a7564f8e29c4e54408
anderspapitto <anderspapitto@gmail.com>
2025-04-01 12:26:27 -0700

    remove anderspapitto as maintainer

--- pkgs/by-name/di/dirt/package.nix
+++ pkgs/by-name/di/dirt/package.nix
@@ -47,7 +47,7 @@ stdenv.mkDerivation {
     description = "Unimpressive thingie for playing bits of samples with some level of accuracy";
     homepage = "https://github.com/tidalcycles/Dirt";
     license = licenses.gpl3;
-    maintainers = with maintainers; [ anderspapitto ];
+    maintainers = with maintainers; [];
     platforms = with platforms; linux;
     mainProgram = "dirt";
   };
